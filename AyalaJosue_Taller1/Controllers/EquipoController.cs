79cb9efbba6eea1f9b2506586727cddac5b97b56
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2025-04-11 15:23:50 -0500

    Agregar archivos de proyecto.

---
+++ AyalaJosue_Taller1/Controllers/EquipoController.cs
@@ -0,0 +1,50 @@
+﻿using AyalaJosue_Taller1.Models;
+using AyalaJosue_Taller1.NewFolder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+namespace AyalaJosue_Taller1.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public EquipoRepository _repository;
+        public EquipoController()
+        {
+            _repository = new EquipoRepository();
+        }
+        public ActionResult View()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+        
+            var equipos = _repository.DevuelveListadoEquipos();
+            return View(equipos);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var equipo = _repository.DevuelveEquiposPorID(id);
+            return View(equipo);
+        }
+        [HttpPost]
+        public ActionResult Edit(int Id, Equipo equipo)
+        {
+            //Proceso de guardar
+            try
+            {
+                _repository.ActializarEquipo(Id, equipo);
+                return RedirectToAction(nameof(List));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AyalaJosue_Taller1/Repository/EquipoRepository.cs
@@ -0,0 +1,48 @@
+﻿using AyalaJosue_Taller1.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AyalaJosue_Taller1.NewFolder
+{
+    public class EquipoRepository
+
+    {
+        public IEnumerable<Equipo> DevuelveListadoEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "Liga de quito",
+                PartidosJugados = 10,
+                PartidosGanamos = 10,
+                PartidosEmpatadaso = 10,
+                PartidosPerdidos = 10,
+
+            };
+            Equipo barcelona = new Equipo
+            {
+                Id = 2,
+                Nombre = "Barcelona",
+                PartidosJugados = 10,
+                PartidosGanamos = 7,
+                PartidosEmpatadaso = 2,
+                PartidosPerdidos = 1,
+
+            };
+            equipos.Add(ldu);
+            equipos.Add(barcelona);
+            return equipos;
+        }
+        public Equipo DevuelveEquiposPorID(int Id)
+        {
+            var equipos = DevuelveListadoEquipos();
+            var equipo = equipos.First(item=> item.Id == Id);
+            return equipo;
+        }
+        public bool ActializarEquipo(int id, Equipo equipo)
+        {
+            return true;
+
+        }
+    }
+}
